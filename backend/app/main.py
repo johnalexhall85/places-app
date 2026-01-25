5ff8aa72675af6559a91bef4c7db67076075ff53
johnalexhall85 <johnalexanderhall@gmail.com>
2026-01-25 09:46:23 -0600

    Add measures API endpoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,8 +2,10 @@ from fastapi import FastAPI
 
 from app.routers.health import router as health_router
 from app.routers.counties import router as counties_router
+from app.routers.measures import router as measures_router
 
 app = FastAPI(title="PLACES (independent) API", version="0.1.0")
 
 app.include_router(health_router)
 app.include_router(counties_router)
+app.include_router(measures_router)
